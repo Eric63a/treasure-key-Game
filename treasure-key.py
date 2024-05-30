a3323bc24e8a5c80e6b4916b278b91c8ba4a135c
Suksharkki <135727650+Eric63a@users.noreply.github.com>
2024-05-30 20:27:46 +0700

    first commit

---
+++ treasure-key.py
@@ -0,0 +1,69 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+
+print('''
+     8 8 8 8                     ,ooo.
+     8a8 8a8                    oP   ?b
+    d888a888zzzzzzzzzzzzzzzzzzzz8     8b
+     `""^""'                    ?o___oP'
+      ''') 
+
+
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+choice1 = input("This is your first choice, I have only 2 you choose If you choose wrong, it's game over\n left or right? ") 
+if choice1 == "left":
+    print ('You pass next question')
+elif choice1 == "right":
+    print ("Fall into a hole.\n Game Over.")
+    exit()
+
+choice2 = input("This is your Second choice, I have only 2 you choose If you choose wrong, it's game over.\n  swim or wait? ")
+
+if choice2 == "Wait":
+    print ("You pass next question")
+elif choice2 == "swim":
+    print("Attacked by trout. Game Over." )
+    exit()
+
+choice3 = input ("This is the last question., I have 3 doors, 3 three colors you choose If you choose wrong, it's game over.\n Which door? Yellow, Red and blue  ")
+
+if choice3 == "Yellow":
+    print ("\nYou Win!")
+elif choice3 == "Red":
+    print ("\nBurned by fire.\n Game Over.")
+    exit()
+elif choice3 == "blue":
+    print ("\nEaten by beasts.\n Game Over.")
+    exit()
+else:
+    print ("\nGame Over.")
+
+
+
+
